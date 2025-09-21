5d0973e568c18116a709533b6877bf57bd8ee888
Chopapik <123145310+Chopapik@users.noreply.github.com>
2025-09-21 22:21:52 +0200

    Add default avatar and support user avatars in PostCard

--- client/src/features/postCards/components/PostCard.tsx
+++ client/src/features/postCards/components/PostCard.tsx
@@ -1,29 +1,29 @@
 ﻿import React from "react";
+import DefaultAvatar from "@/assets/defaultAvatar.svg?react";
 
 export type Post = {
   id: string | number;
   userName: string;
+  userAvatarUrl: string;
   date: string;
   device: string;
   body: string;
 };
 
+const avatarClass = "size-10 rounded-[10px] border border-neutral-800";
+
 export const PostCard: React.FC<{ post: Post }> = ({ post }) => {
   return (
-    <div
-      data-hasimage="true"
-      data-hastext="true"
-      data-size="Compact"
-      className="w-full max-w-[920px] p-3 bg-primary-neutral-background-default rounded-[10px] border-t-2 border-primary-neutral-stroke-default inline-flex flex-col justify-start items-start gap-5 overflow-hidden"
-    >
-      <div
-        data-size="Compact"
-        className="w-72 inline-flex justify-start items-center gap-2"
-      >
-        <img
-          className="size-10 rounded-[10px] border border-neutral-800"
-          src="https://placehold.co/40x40"
-        />
+    <div className="w-full max-w-[920px] p-3 bg-primary-neutral-background-default rounded-[10px] border-t-2 border-primary-neutral-stroke-default inline-flex flex-col justify-start items-start gap-5 overflow-hidden">
+      <div className="w-72 inline-flex justify-start items-center gap-2">
+        <div className="size-10 rounded-[10px] border border-neutral-800">
+          {post.userAvatarUrl ? (
+            <img className={avatarClass} src={post.userAvatarUrl} />
+          ) : (
+            <DefaultAvatar className={avatarClass} />
+          )}
+        </div>
+
         <div className="self-stretch inline-flex flex-col justify-center items-start">
           <div className="justify-start text-text-main text-sm font-medium font-['Roboto']">
             {post.userName}
@@ -39,10 +39,7 @@ export const PostCard: React.FC<{ post: Post }> = ({ post }) => {
           </div>
         </div>
       </div>
-      <div
-        data-size="Compact"
-        className="self-stretch flex flex-col justify-center items-center gap-2.5 overflow-hidden"
-      >
+      <div className="self-stretch flex flex-col justify-center items-center gap-2.5 overflow-hidden">
         <div className="self-stretch justify-start text-text-main text-sm font-normal font-['Roboto']">
           {post.body}
         </div>
@@ -54,15 +51,8 @@ export const PostCard: React.FC<{ post: Post }> = ({ post }) => {
         />
       </div>
 
-      <div
-        data-property-1="Default"
-        className="w-24 px-2 bg-white/0 inline-flex justify-start items-start gap-2 overflow-hidden"
-      >
-        <div
-          data-animseq="1"
-          data-status="Unliked"
-          className="size- flex justify-start items-center gap-2"
-        >
+      <div className="w-24 px-2 bg-white/0 inline-flex justify-start items-start gap-2 overflow-hidden">
+        <div className="size- flex justify-start items-center gap-2">
           <div className="size-6 relative overflow-hidden">
             <div className="size-5 left-[2px] top-[3px] absolute bg-text-neutral" />
           </div>
@@ -70,10 +60,7 @@ export const PostCard: React.FC<{ post: Post }> = ({ post }) => {
             1
           </div>
         </div>
-        <div
-          data-theme="Dark"
-          className="h-6 flex justify-start items-center gap-2"
-        >
+        <div className="h-6 flex justify-start items-center gap-2">
           <div className="size-6 relative overflow-hidden">
             <div className="size-4 left-[3px] top-[3px] absolute outline-[2.50px] outline-offset-[-1.25px] outline-text-neutral" />
           </div>
