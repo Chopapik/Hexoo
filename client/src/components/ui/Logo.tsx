c291e9a472518f88ae36e999656248117629e8e6
Krzysztof <123145310+Chopapik@users.noreply.github.com>
2025-10-20 16:39:02 +0200

    add ui components

---
+++ client/src/components/ui/Logo.tsx
@@ -0,0 +1,16 @@
+import LogoSvg from "@/assets/Logo.svg?react";
+import LogoSmSvg from "@/assets/LogoSm.svg?react";
+
+export function Logo() {
+  return (
+    <>
+      {/* <div className="hidden sm:block">
+        <LogoSvg />
+      </div>
+      <div className="sm:hidden">
+        <LogoSmSvg />
+      </div> */}
+      <span>etest</span>
+    </>
+  );
+}
