918803d412f4eaf9a4be283263f6312fa3552f0a
Krzysztof <123145310+Chopapik@users.noreply.github.com>
2025-10-22 18:40:50 +0200

    Remove legacy client_old directory

--- client_old/src/hooks/layout/useMainPage.tsx
+++
@@ -1,3 +0,0 @@
-const useChangeMain = () => {
-  return [];
-};
--- client_old/src/pages/HomePage.tsx
+++
@@ -1,5 +0,0 @@
-﻿import { PostList } from "@/features/posts/components/PostList";
-
-export function HomePage() {
-  return <PostList />;
-}
