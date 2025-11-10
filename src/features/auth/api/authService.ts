5422ef29d67ce82cd429f633909056db4f21e20c
Krzysztof <123145310+Chopapik@users.noreply.github.com>
2025-11-10 21:50:29 +0100

    Refactor: split registerUser into smaller functions

--- src/features/auth/api/authService.ts
+++ src/features/auth/api/authService.ts
@@ -1,15 +1,25 @@
-import { adminAuth, adminDb } from "@/lib/firebaseAdmin";
+import { adminAuth } from "@/lib/firebaseAdmin";
 import { RegisterData } from "../types/auth.types";
-import admin from "firebase-admin";
-import { FirebaseAuthError } from "firebase-admin/auth";
-import handleRegisterError from "./handleRegisterError";
-import { validateField } from "../utils/registerValidation";
+import { createUserDocument } from "@/features/users/api/userService";
+import { handleRegistrationError } from "./processRegistrationError";
+
+export async function createAuthUser(userData: {
+  email: string;
+  password: string;
+  displayName: string;
+}) {
+  return adminAuth.createUser({
+    email: userData.email,
+    password: userData.password,
+    displayName: userData.displayName,
+  });
+}
 
 export async function registerUser(userRegisterData: RegisterData) {
   let uid: string | null = null;
 
   try {
-    const user = await adminAuth.createUser({
+    const user = await createAuthUser({
       email: userRegisterData.email,
       password: userRegisterData.password,
       displayName: userRegisterData.name,
@@ -17,46 +27,15 @@ export async function registerUser(userRegisterData: RegisterData) {
 
     uid = user.uid;
 
-    await adminDb.doc(`users/${uid}`).set(
-      {
-        uid,
-        name: userRegisterData.name,
-        email: userRegisterData.email,
-        role: "user",
-        createdAt: admin.firestore.FieldValue.serverTimestamp(),
-      },
-      { merge: true }
-    );
+    await createUserDocument(uid, {
+      name: userRegisterData.name,
+      email: userRegisterData.email,
+      role: "user",
+    });
 
     const token = await adminAuth.createCustomToken(uid);
     return { ok: true, token };
   } catch (error) {
-    if (uid) {
-      try {
-        await adminAuth.deleteUser(uid);
-        console.warn(
-          `Rollback: użytkownik ${uid} został usunięty po błędzie Firestore.`
-        );
-      } catch (rollbackError) {
-        console.error("Błąd podczas rollbacku użytkownika:", rollbackError);
-      }
-    }
-
-    if (error instanceof FirebaseAuthError) {
-      throw {
-        ok: false,
-        data: handleRegisterError(error),
-        type: "validation",
-        code: 400,
-      };
-    }
-
-    console.error("Błąd rejestracji użytkownika:", error);
-    throw {
-      ok: false,
-      message: "Wystąpił błąd serwera",
-      type: "critical",
-      code: 500,
-    };
+    await handleRegistrationError(error, uid);
   }
 }
