2b2c5223b231076ef94728280f714f42956c94fa
Krzysztof <123145310+Chopapik@users.noreply.github.com>
2025-11-13 23:03:49 +0100

    Implement changing user's password by admin

---
+++ src/app/api/admin/updateUserPassword/[uid]/route.ts
@@ -0,0 +1,20 @@
+import { updateUserPassword } from "@/features/admin/api/adminService";
+
+export async function PUT(
+  req: Request,
+  context: { params: Promise<{ uid: string }> }
+) {
+  try {
+    const body = await req.json();
+    const { uid } = await context.params;
+
+    const { newPassword } = body;
+
+    const result = await updateUserPassword(uid, newPassword);
+
+    return Response.json({ ok: true }, { status: 200 });
+  } catch (error) {
+    console.error("PUT user error:", error);
+    return Response.json({ ok: false }, { status: 500 });
+  }
+}
