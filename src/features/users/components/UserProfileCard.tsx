69a9cc9253f64ef573f4e14ae17b64c33d25a665
Krzysztof <123145310+Chopapik@users.noreply.github.com>
2025-11-22 23:12:31 +0100

    Refactor modal and user profile UI styles

--- src/features/users/components/UserProfileCard.tsx
+++ src/features/users/components/UserProfileCard.tsx
@@ -62,40 +62,37 @@ export const UserProfileCard = ({
           onClose={() => setShowEditProfileModal(false)}
         />
       )}
-      <div
-        data-type="Default"
-        className="self-stretch w-full p-4 md:px-6 md:py-5 bg-primary-neutral-background-default rounded-[10px] border-t border-primary-neutral-stroke-default inline-flex flex-col md:flex-row justify-start items-center gap-3 relative"
-      >
+      <div className="self-stretch w-full p-4 md:px-6 md:py-5 bg-primary-neutral-background-default rounded-xl border-t-2 border-primary-neutral-stroke-default inline-flex flex-col md:flex-row justify-start items-center gap-3 relative">
         <Image
-          className="w-16 h-16 xs:w-24 xs:h-24 rounded-[10px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border border-secondary-neutral-background-default object-cover"
+          className="w-16 h-16 xs:w-24 xs:h-24 rounded-xl shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border border-secondary-neutral-background-default object-cover"
           src={avatarUrl ?? defaultAvatarUrl}
           alt={`${name}'s avatar`}
           width={96}
           height={96}
         />
 
-        <div className="flex-1 w-full  inline-flex flex-col md:justify-center items-start gap-2 overflow-hidden">
-          <div className="flex flex-col md:flex-row md:justify-between md:items-center text-center md:text-left w-full text-text-main text-base xs:text-2xl font-bold font-['Roboto'] truncate">
-            <span>{name}</span>
+        <div className="flex-1 w-full  inline-flex flex-col md:justify-center items-start gap-2 overflow-hidden ">
+          <div className="flex flex-col md:px-1 md:flex-row md:justify-between md:items-center text-center md:text-left w-full text-text-main text-base xs:text-2xl font-bold font-Albert_Sans truncate">
+            <span className="ml-1">{name}</span>
             {enableSettings && (
               <Button
                 text="edytuj profil"
                 size="sm"
-                variant="glass-card"
-                className="bg-neutral-500/10"
+                // variant="glass-card"
+                // className="bg-neutral-500/10"
                 onClick={() => setShowEditProfileModal(true)}
               />
             )}
           </div>
 
           {/* Info bar */}
-          <div className="self-stretch  px-5 py-3.5 bg-[radial-gradient(ellipse_113.20%_442.25%_at_26.12%_10.28%,_var(--text-main,_rgba(255,_255,_255,_0.04))_0%,_var(--text-neutral,_rgba(115,_115,_115,_0.04))_100%)] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-xl inline-flex flex-col md:flex-row justify-center md:justify-between items-center gap-5 md:gap-4 overflow-hidden">
+          <div className="self-stretch  px-5 py-3.5 bg-[radial-gradient(ellipse_113.20%_442.25%_at_26.12%_10.28%,var(--text-main,rgba(255,255,255,0.04))_0%,var(--text-neutral,rgba(115,115,115,0.04))_100%)] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-xl inline-flex flex-col md:flex-row justify-center md:justify-between items-center gap-5 md:gap-4 overflow-hidden">
             <div className="w-full md:w-auto inline-flex justify-center md:justify-start items-center gap-3 md:gap-4">
               <div className="inline-flex flex-col justify-start items-start gap-0.5">
-                <div className="self-stretch h-3.5 justify-start text-text-neutral text-xs font-bold font-['Roboto']">
+                <div className="self-stretch h-3.5 justify-start text-text-neutral text-xs font-bold font-Albert_Sans">
                   dołączył
                 </div>
-                <div className="self-stretch h-3.5 justify-start text-text-main text-xs font-normal font-['Roboto']">
+                <div className="self-stretch h-3.5 justify-start text-text-main text-xs font-normal font-Albert_Sans">
                   {formatJoinDate(createdAt)}
                 </div>
               </div>
@@ -105,10 +102,10 @@ export const UserProfileCard = ({
                   <span className="w-px h-5 bg-neutral-500 inline-block" />
 
                   <div className="inline-flex flex-col justify-start items-start gap-0.5">
-                    <div className="self-stretch h-3.5 justify-start text-text-neutral text-xs font-bold font-['Roboto']">
+                    <div className="self-stretch h-3.5 justify-start text-text-neutral text-xs font-bold font-Albert_Sans">
                       ostatnio online
                     </div>
-                    <div className="self-stretch h-3.5 justify-start text-text-main text-xs font-normal font-['Roboto']">
+                    <div className="self-stretch h-3.5 justify-start text-text-main text-xs font-normal font-Albert_Sans">
                       {/* {formatLastOnlineDate(lastOnline)} */}
                     </div>
                   </div>
@@ -117,18 +114,15 @@ export const UserProfileCard = ({
               {/* <span className="hidden xs:inline-block w-px h-5 bg-neutral-500" />
 
               <div className="hidden xs:inline-flex w-24 flex-col justify-start items-start gap-0.5">
-                <div className="self-stretch justify-start text-text-neutral text-xs font-bold font-['Roboto']">
+                <div className="self-stretch justify-start text-text-neutral text-xs font-bold font-Albert_Sans">
                   liczba postów
                 </div>
-                <div className="self-stretch justify-start text-text-main text-xs font-normal font-['Roboto']"></div>
+                <div className="self-stretch justify-start text-text-main text-xs font-normal font-Albert_Sans"></div>
               </div> */}
             </div>
 
-            <div
-              data-status={status}
-              className="h-8 flex justify-center items-center overflow-hidden md:ml-auto"
-            >
-              <div className="justify-start text-text-neutral text-lg font-medium font-['Roboto'] italic">
+            <div className="h-8 flex justify-center items-center overflow-hidden md:ml-auto">
+              <div className="justify-start text-text-neutral text-lg font-medium font-Albert_Sans italic mr-1">
                 {status}
               </div>
             </div>
