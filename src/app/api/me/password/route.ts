db2346d1773a3b8baf42255e6df1501a1421dd00
Krzysztof <123145310+Chopapik@users.noreply.github.com>
2025-11-27 23:20:11 +0100

    Add google recaptcha provider

--- src/app/api/me/password/route.ts
+++ src/app/api/me/password/route.ts
@@ -1,9 +1,22 @@
 import { updatePassword } from "@/features/me/api/meService";
 import { withErrorHandling } from "@/lib/http/routeWrapper";
 import { handleSuccess } from "@/lib/http/responseHelpers";
+import { verifyRecaptchaToken } from "@/lib/recaptcha";
+import { createAppError } from "@/lib/ApiError";
 
 export const PUT = withErrorHandling(async (req: Request) => {
   const body = await req.json();
+
+  const { recaptchaToken } = body;
+
+  if (!recaptchaToken) {
+    throw createAppError({
+      code: "FORBIDDEN",
+      message: "Brak weryfikacji reCAPTCHA",
+    });
+  }
+
+  await verifyRecaptchaToken(recaptchaToken);
 
   const result = await updatePassword(body);
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,10 @@ import "@/styles/scrollbar.css";
 import "@/styles/glassmorphism.css";
 import ReduxProvider from "@/lib/providers/ReduxProvider";
 import QueryProvider from "@/lib/providers/QueryProvider";
+import RecaptchaProvider from "@/lib/providers/RecaptchaProvider";
 export const metadata: Metadata = {
   title: "Hexoo",
 };
-
 export default function RootLayout({
   children,
 }: {
@@ -17,9 +17,33 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <body>
-        <QueryProvider>
-          <ReduxProvider>{children}</ReduxProvider>
-        </QueryProvider>
+        <RecaptchaProvider>
+          <QueryProvider>
+            <ReduxProvider>{children}</ReduxProvider>
+          </QueryProvider>
+        </RecaptchaProvider>
+
+        <footer className="w-full py-6 text-center text-xs text-text-neutral opacity-70">
+          Ta strona jest chroniona przez reCAPTCHA, a obowiązują zasady
+          <a
+            href="https://policies.google.com/privacy"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Polityki prywatności
+          </a>
+          oraz
+          <a
+            href="https://policies.google.com/terms"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Warunki korzystania z usług Google
+          </a>
+          .
+        </footer>
       </body>
     </html>
   );
