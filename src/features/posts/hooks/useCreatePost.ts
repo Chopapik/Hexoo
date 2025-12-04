6a9eb6815d610eda88554b0235847764f5f12409
Krzysztof <123145310+Chopapik@users.noreply.github.com>
2025-12-04 21:41:19 +0100

    Add posts data invalidation to create-post logic

--- src/features/posts/hooks/useCreatePost.ts
+++ src/features/posts/hooks/useCreatePost.ts
@@ -1,11 +1,13 @@
 import axiosInstance from "@/lib/axiosInstance";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { CreatePost } from "../types/post.type";
 
 export default function useCreatePost(
   successFnCallBack?: (data?: any) => void,
   errorFnCallBack?: (err?: any) => void
 ) {
+  const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: async (postData: CreatePost | FormData) => {
       if (postData instanceof FormData) {
@@ -22,7 +24,10 @@ export default function useCreatePost(
       return res.data;
     },
 
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
+      await queryClient.invalidateQueries({
+        queryKey: ["posts"],
+      });
       successFnCallBack?.(data);
     },
 
