5c647f24c3c6a118e55a1096c601970bca0d36ca
Krzysztof <123145310+Chopapik@users.noreply.github.com>
2026-01-14 22:14:46 +0100

    Fix cleaning brute force data after succesfull login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -4,6 +4,7 @@ import { handleSuccess } from "@/lib/http/responseHelpers";
 import { createAppError } from "@/lib/AppError";
 import { verifyRecaptchaToken } from "@/lib/recaptcha";
 import { NextRequest } from "next/server";
+import { getClientIp } from "@/lib/serverUtils";
 
 export const POST = withErrorHandling(async (req: NextRequest) => {
   const body = await req.json();
@@ -25,7 +26,9 @@ export const POST = withErrorHandling(async (req: NextRequest) => {
 
   await verifyRecaptchaToken(recaptchaToken);
 
-  const result = await createSession(idToken);
+  const ip = await getClientIp();
+
+  const result = await createSession(idToken, ip);
 
   return handleSuccess(result);
 });
